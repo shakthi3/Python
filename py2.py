842b6a8baadf0539b7eb861aa08b1a46cf76deae
shakthi3 <meena2672005@gmail.com>
2026-07-21 12:08:25 +0530

    Add files via upload

---
+++ py2.py
@@ -0,0 +1,13 @@
+
+tamil= int(input("enter your tamil_marks"))
+english= int(input("enter your english_marks"))
+science= int(input("enter your science_marks"))
+
+
+total_marks =tamil + english +science
+average_mark = (tamil + english +science) /3
+
+
+
+print(total_marks)
+print(average_mark)
---
+++ py3.py
@@ -0,0 +1,8 @@
+
+
+parking_hr = int(input("enter your parking hour: "))
+first = 30
+second = (parking_hr -2 )*20
+total_hr = first + second
+print("enter the total amount: ", total_hr)
+
